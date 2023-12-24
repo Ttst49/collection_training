af08c95232642f74825e42620b0f3780d1c7da86
Ttst <thibautstachnick@gmail.com>
2023-12-25 00:00:20 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+fn vector_training(){
+
+    //create a vector without values
+    let mut v : Vec<i64> = Vec::new();
+
+    //shortcut vec! create a vector with values inside
+    let w = vec![12,27,92];
+
+    // .push to add values to a vector
+    v.push(12);
+    v.push(736);
+    v.push(2);
+    v.push(73);
+    v.push(01);
+
+}
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
+
+
+
